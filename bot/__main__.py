5de5ee066d8baa2fee861bcd5bf73184f29506a3
Mafia58 <90953455+Mafia58@users.noreply.github.com>
2021-10-30 16:21:55 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -54,17 +54,17 @@ This bot can mirror all your links to Google Drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Support Group", "https://telegram.dog/cloudsmirror")
+    buttons.buildbutton("Support Group", "https://t.me/DFX_LEECH")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     LOGGER.info('UID: {} - UN: {} - MSG: {}'.format(update.message.chat.id, update.message.chat.username, update.message.text))
     uptime = get_readable_time((time.time() - botStartTime))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         if update.message.chat.type == "private" :
-            sendMessage(f"Hey I'm Alive 🙂\nSince: <code>{uptime}</code>", context.bot, update)
+            sendMessage(f"Hey Besike kolla. Marila na itim. dnno ne @akalankanime Keriyek tmi. 🙂\nSince: <code>{uptime}</code>", context.bot, update)
         else :
             update.effective_message.reply_photo(IMAGE_URL, start_string, parse_mode=ParseMode.MARKDOWN, reply_markup=reply_markup)
     else :
-        sendMessage(f"Oops! not a Authorized user.", context.bot, update)
+        sendMessage(f"Hey Besike kolla. Marila na itim. dnno ne @akalankanime Keriyek tmi. ubata botwa use krnna ba. sorry pocca", context.bot, update)
 
 
 def restart(update, context):
